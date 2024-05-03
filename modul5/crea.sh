2289a4bd81e73c6b1fc48507262e8f8d628fe431
alviboi <alviboi@gmail.com>
2024-05-03 20:18:37 +0200

    m5

--- modul5/crea.sh
+++ modul5/crea.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-pandoc M2.md -o M2.pdf --from markdown+implicit_figures --template eisvogel --listings --filter pandoc-latex-environment --number-sections --toc
-pandoc M2_es.md -o M2_es.pdf --from markdown+implicit_figures --template eisvogel --listings --filter pandoc-latex-environment --number-sections --toc
+pandoc M5.md -o M5.pdf --from markdown+implicit_figures --template eisvogel --listings --filter pandoc-latex-environment --number-sections --toc
+pandoc M5_es.md -o M5_es.pdf --from markdown+implicit_figures --template eisvogel --listings --filter pandoc-latex-environment --number-sections --toc
--- modul5/tot.sh
+++ modul5/tot.sh
@@ -2,5 +2,5 @@
 ./crea.sh
 ./crea_html.sh
 # zip the files aqua.css index.html and the folders img i rsc
-zip -r M2.zip index.html aqua.css img rsc
-zip -r M2_es.zip index_es.html aqua.css img rsc
+zip -r M5.zip index.html aqua.css img rsc
+zip -r M5_es.zip index_es.html aqua.css img rsc
